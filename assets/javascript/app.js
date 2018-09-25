3a7224285efd103786c98a20f62f191ad35871be
FORTRESS <alexpetroski@gmail.com>
2018-09-25 18:27:08 -0400

    Buttons working, but not added buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,28 @@
-var topics = []
+$(document).ready(function(){
 
+    $('.gif-buttons').on("click", function(){
+        var x = $(this).data("search");
 
-$('.gif-buttons').on("click", function(){
-    var x = $(this).data("search");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q="+x+"&api_key=8oRpogwv4c8vkFUetQ2qfgXpoW6s8k8K&limit=10";
+        console.log(queryURL);
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+x+"&api_key=8oRpogwv4c8vkFUetQ2qfgXpoW6s8k8K&limit=10";
-    console.log(queryURL);
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var results = response.data
+            console.log(results[0].rating);
+            for (let i = 0; i < results.length; i++) {
+                $("#gif-div").prepend("<p>Rating: " + results[i].rating + "</p>");
+                $("#gif-div").prepend("<img src='" + results[i].images.downsized.url + "'>")
+            }
+        });
+    });
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        var results = response.data
-        console.log(results[0].rating);
-        for (let i = 0; i < results.length; i++) {
-            $("#gif-div").prepend("<p>Rating: " + results[i].rating + "</p>");
-            $("#gif-div").prepend("<img src='" + results[i].images.downsized.url + "'>")
-        }
+    $('#search').on("click", function () {
+        var value = $("#searchvalue").val().trim();
+        var newButton = ("<button class='gif-buttons' data-search="+value+">" + value + "</button>")
+        $("#button-div").append(" " + newButton + " ");
     });
 });
-
-$('#search').on("click", function () {
-    var value = $("#searchvalue").val().trim();
-    var newButton = ("<button class='gif-buttons' data-search=" + value + ">" + value + "</button>")
-    $("#buttondiv").append(" " + newButton + " ");
-});
-
-
 
